f69d28e01a3580f5a32d494cf79970bb19e04c46
r12000 <romaaksenov2008@gmail.com>
2026-06-07 14:50:26 +0300

    Add files via upload

---
+++ ДЕЛЕНИЕ/script.js
@@ -0,0 +1,12 @@
+function even(n, x, y) {
+   if (x > 0 || y > 0) {
+    if (z = (n / x) % y == 0) {
+        console.log(true)
+    }
+    else (
+        console.log(false)
+    )
+   }
+}
+
+even(3, 24, 1)
